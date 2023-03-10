2015591f55a24b5a1988c4dee6c7619800d7ad13
Self-taught <63093667+Self-taught@users.noreply.github.com>
2023-03-10 13:16:43 +0530

    Added Url endpoints for yearly and all data.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
+df_st = pd.read_csv("data_small/stations.txt", skiprows=17)
+df_st = df_st[["STAID", "STANAME                                 "]][:20]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=df_st.to_html(index=False))
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -19,6 +22,23 @@ def about(station, date):
         "date": date,
         "temperature": temperature
     }
+
+
+@app.route("/api/v1/<station>")
+def all_records(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df[:50].to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/year/<station>/<year>")
+def annual_data(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
